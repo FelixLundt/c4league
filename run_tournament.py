f3d776975d4903cf2546b287a0ccf92659f33c86
FelixLundt <felixlundt@gmail.com>
2025-02-14 20:06:21 +0100

    Added script to run tournament.

---
+++ run_tournament.py
@@ -0,0 +1,65 @@
+"""
+This script is used to run a tournament.
+"""
+from pathlib import Path
+from c4league.tournament_manager import TournamentManager
+from c4league.storage.cloud_storage import get_submitted_agents
+from c4league.utils import TournamentPlayer
+from c4league.utils import get_new_agents, get_updated_agents, get_containerized_agents
+from c4league.utils import containerize_agents
+
+
+def run_tournament():
+    # Make sure everything is set up correctly
+    print('Checking if everything is set up correctly...')
+    # Check if the directory containing this script contains a .env file
+    if not Path('.env').exists():
+        print('Error: .env file not found in the directory containing this script.')
+        exit(1)
+    # Check if the directory contains a run_match.sif file
+    if not Path('run_match.sif').exists():
+        print('Error: run_match.sif file not found in the directory containing this script.')
+        exit(1)
+
+    # Get participants
+    print('Gathering tournament participants...')
+
+    print('Getting submitted agents from cloud storage...')
+    submitted_agents = [TournamentPlayer(**agent) for agent in get_submitted_agents()]
+    print('Checking for new agents...')
+    new_agents = get_new_agents(submitted_agents, get_containerized_agents())
+    print('Checking for updated agents...')
+    updated_agents = get_updated_agents(submitted_agents, get_containerized_agents())
+    print(f'Found {len(new_agents)} new agents and {len(updated_agents)} updated agents.')
+
+    # Build new/updated agents
+    print('Building new/updated agents...')
+    try:
+        containerize_agents(new_agents + updated_agents)
+    except Exception as e:
+        print(f'Error building agents: {e}')
+        exit(1)
+
+    # Set up tournament manager
+    # Needs to:
+    # - Initialize tournament (get new id, localize results dir, ..)
+    # - Generate pairings (sets of TournamentPlayers/paths to agents with match ids)
+    # - Submit all matches
+    # - Wait for all matches to complete
+    # - Process results
+
+    # Each match is a Slurm job
+    # - We need to set up a script to run the match
+    # - The match needs to run several games and store the results (game results, match results)
+
+
+    manager = TournamentManager()
+
+    # Run tournament
+    print('Running tournament...')
+    manager = TournamentManager()
+
+    manager.run_tournament()
+
+if __name__ == "main":
+    run_tournament()
