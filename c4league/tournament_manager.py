aea60ae072458a03c33485651e4d6f75d5ae038c
FelixLundt <felixlundt@gmail.com>
2025-02-16 01:20:58 +0100

    Try with fakeroot.

--- c4league/tournament_manager.py
+++ c4league/tournament_manager.py
@@ -236,14 +236,16 @@ ls -la
 
 # Mount the script, the c4utils package, the results directory and the sif files at runtime
 apptainer exec \\
-    --privileged \\
     --bind {str(self.c4league_package_root)}:/opt/c4league \\
     --bind {os.getenv("C4UTILS_DIR")}:/opt/c4utils \\
     --bind {os.getenv("C4LEAGUE_ROOT_DIR")}/run_match.py:/opt/run_match.py \\
     --bind {str(self.results_dir)}/$match_id:/opt/match_results/ \\
     --bind $agent1_path:/opt/$agent1_name \\
     --bind $agent2_path:/opt/$agent2_name \\
-    --contain \\
+    --fakeroot \\
+    --writable-tmpfs \\
+    --net \\
+    --containall \\
     run_match.sif \\
     python3 /opt/run_match.py \\
         --agent-paths "/opt/$agent1_name" "/opt/$agent2_name" \\
