326fe08701825f7c4276e89618319ff24d12ff23
FelixLundt <felixlundt@gmail.com>
2025-02-16 09:38:09 +0100

    Added missing PATH variable in container build, fixed reference to run_match script

--- c4league/tournament_manager.py
+++ c4league/tournament_manager.py
@@ -243,7 +243,7 @@ echo "Agent 2: $agent2_path -> $agent2_name"
 apptainer exec \\
     --bind {str(self.c4league_package_root)}:/opt/c4league \\
     --bind {os.getenv("C4UTILS_DIR", "/opt/c4utils")}:/opt/c4utils \\
-    --bind {str(self.c4league_package_root)}/run_match.py:/opt/run_match.py \\
+    --bind {os.getenv("C4LEAGUE_ROOT_DIR")}/run_match.py:/opt/run_match.py \\
     --bind {str(self.results_dir)}/$match_id:/opt/match_results/ \\
     --bind $agent1_path:/opt/$agent1_name \\
     --bind $agent2_path:/opt/$agent2_name \\
